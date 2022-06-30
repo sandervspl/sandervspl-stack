cbad2eb25d747ca7ccc4e95dff56ad23995839a6
Sander Vispoel <sandervispoel@hotmail.com>
2022-06-30 14:46:37 +0200

    Add prettier-sort plugin and patch

--- prettier.config.js
+++ prettier.config.js
@@ -4,4 +4,16 @@ module.exports = /** @type {import('prettier').Config} */ ({
   arrowParens: 'always',
   singleQuote: true,
   printWidth: 100,
+  importOrder: [
+    '^types$',
+    '^(react|react-dom)$',
+    '^next(.*)$',
+    '<THIRD_PARTY_MODULES>',
+    '<BLANK_LINE>',
+    '^(vectors|services|hooks|queries|styles)(/.*|$)',
+    '^(pages|layouts|modules|common)(/.*|$)',
+    '<BLANK_LINE>',
+    '^[./]',
+  ],
+  plugins: [require('@trivago/prettier-plugin-sort-imports')],
 });
